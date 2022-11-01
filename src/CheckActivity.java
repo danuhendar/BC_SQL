ae1c690f964a7ade3f4a37cb88ea9bb2ae3d59ba
danuhendar <danuhendar14@gmail.com>
2022-11-01 17:18:02 +0700

    Perubahan metoder restart backend
    Perubahan dari menggunakan ListenerBackend ke Runtime
    
    Co-Authored-By: danu <15968+danu@users.noreply.github.com>

--- src/CheckActivity.java
+++ src/CheckActivity.java
@@ -49,6 +49,17 @@ public class CheckActivity {
         return res;
     }
 	
+	public void Restart_Service(String command,String nama_service) {
+		try {
+			Runtime r = Runtime.getRuntime();
+			Process proc = r.exec(command);
+			System.out.println("command : "+command);
+			gf.WriteLog("Restart "+nama_service+" : SUKSES", true);
+		}catch(Exception exc) {
+			
+		}
+	}
+	
 	
 	public void Run() {
 		try {
@@ -60,8 +71,9 @@ public class CheckActivity {
 			System.err.println("last_message : "+get_last_time_message_incoming+" VS waktu_kini : "+waktu_kini+" Sel. : "+selisih);
 			String selisih_menit = selisih.split(":")[1];
 			if(Integer.parseInt(selisih_menit) > batas_menit) {
-				String command = "systemctl restart BC_SQL_"+kode_cabang;
-				RestartIDMReporter_by_service("LISTENER_BACKEND_523/",command,"BC_SQL");
+				String command = "systemctl restart "+gf.en.getId_reporter();
+				//RestartIDMReporter_by_service("LISTENER_BACKEND_523/",command,"BC_SQL");
+				Restart_Service(command, gf.en.getId_reporter());
 				Thread.sleep(10000);
 				//System.exit(0);
 			}
